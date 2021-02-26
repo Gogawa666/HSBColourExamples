151a937d3f5d4620fb631fd269ebf25089a2dc0f
Russell Gordon <rgordon@lcs.on.ca>
2021-02-26 08:19:19 -0500

    Initial Commit

---
+++ HSBColourExamples/HSBColourExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSBColourExamplesApp.swift
+//  HSBColourExamples
+//
+//  Created by Russell Gordon on 2021-02-26.
+//
+
+import SwiftUI
+
+@main
+struct HSBColourExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
